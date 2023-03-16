941c5559675474e9dd2877d72ed76d3074c57061
David Lohyna <david.lohyna@gmail.com>
2023-03-16 14:37:53 +0100

    Added paginatio to blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.urls import reverse
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
